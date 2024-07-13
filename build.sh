e262834fe3cbd752bb6cb2d354cf5843a16a5d74
mschuppe <vittali@sunrise.ch>
2024-07-13 22:40:11 +0200

    update

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 asciidoctor -D . **/index.adoc
 rsync -a --verbose --perms --times  --prune-empty-dirs --delete-after doc/images .
 rsync -a --verbose --perms --times  --prune-empty-dirs --delete-after doc/pdf .
+$NUAGE/software/tools/linkcheck -e --no-show-redirects localhost:8080/index.html
