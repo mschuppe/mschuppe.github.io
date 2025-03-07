cda4749585a6338e6c5b105f4796b9041a3bdd61
mschuppe <vittali@sunrise.ch>
2025-03-07 23:20:24 +0100

    archive only

--- archive/build.sh
+++ archive/build.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-asciidoctor -D . **/index.adoc
+asciidoctor -D . doc/index.adoc
 rsync -a --verbose --perms --times  --prune-empty-dirs --delete-after doc/images .
 rsync -a --verbose --perms --times  --prune-empty-dirs --delete-after doc/pdf .
 #$NUAGE/software/tools/linkcheck -e --no-show-redirects localhost:8080/index.html
