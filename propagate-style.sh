d28343b8f1cf09636fb8697ccf67fb0ee913c7ad
mschuppe <vittali@sunrise.ch>
2025-03-09 00:47:58 +0100

    no underline

---
+++ propagate-style.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+cp doc/docinfo.html archive/doc
+cp doc/docinfo.html csl/doc
+cp doc/docinfo.html int_essay/doc
+cp doc/docinfo.html mentor/doc
+cp doc/docinfo.html now/doc
+cp doc/docinfo.html path/doc
+cp doc/docinfo.html recording/doc
+cp doc/docinfo.html review/doc
+cp doc/docinfo.html works/doc
